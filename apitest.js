86d1f8bec9bc3664fc6347af4d054efd792b3dd9
avibenfashion <59139276+avibenfashion@users.noreply.github.com>
2020-12-16 20:03:08 +0200

    Update apitest.js

--- apitest.js
+++ apitest.js
@@ -1,1 +1,143 @@
-{"results":[{"uid":"MF05","title":"<p>TROPICAL SHIRTS</p>","custom5":"<p><iframe src=\"http://127.0.0.1:5500/ifashion.html\" width=\"100%\" height=\"200\" ></iframe>","description":"<p><iframe src=\"http://127.0.0.1:5500/ifashion.html\" width=\"100%\" height=\"500\"></iframe></p>","tags":["pcat:All","pcat:FASHION-scat:Shirts","all","Price:50","MF01","colour:Gray"],"images":{},"sort":{"Date":"2020-09-01","Title":"TROPICAL SHIRTS","Price":1,"s1":0,"s2":0,"s3":95,"s4":50},"foot":"","onopenparam":{"title":"TROPICAL SHIRTS","tii_title":"TROPICAL SHIRTS","description":"","video":[],"video_poster":"","carousel_images":[{}],"json_sub_products":[],"cart_url":null,"product_uid":"MF01"},"compiledImg":"http://127.0.0.1:5501/avi/avi.png"},{"uid":"MF06","title":"<p>TROPICAL SHIRTS 06</p>","custom5":"<p><iframe src=\"http://127.0.0.1:5500/ifashion.html\" width=\"100%\" height=\"200\" ></iframe>","description":"<p><iframe src=\"http://127.0.0.1:5500/ifashion.html\" width=\"100%\" height=\"500\"></iframe></p>","tags":["pcat:All","pcat:FASHION-scat:Shirts","all","Price:50","MF01","colour:Gray"],"images":{},"sort":{"Date":"2020-09-01","Title":"TROPICAL SHIRTS","Price":1,"s1":0,"s2":0,"s3":95,"s4":50},"foot":"","onopenparam":{"title":"TROPICAL SHIRTS","tii_title":"TROPICAL SHIRTS","description":"","video":[],"video_poster":"","carousel_images":[{}],"json_sub_products":[],"cart_url":null,"product_uid":"MF01"},"compiledImg":"http://127.0.0.1:5501/avi/avi.png"}],],"coloursPresent":["Blue","Purple","Orange","Red","Yellow","Brown","Beige","Green","Gray","Gold","Pink","Silver","White"]}
+{
+	"results": [{
+		"uid": "MF01",
+		"title": "<p>TROPICAL SHIRTS MF04<\/p>",
+		"custom5":
+		"<p><iframe src=\"https:\/\/ifashion.co\/products\/Sky-Blye-And-Nude-Long-Sleeve-Crop-Top-Two-Piece-Dress-HLBD777\" width=\"525\" height=\"500\"><\/iframe><\/p>",
+		"description": "<p><iframe src=\"https:\/\/ifashion.co\/products\/iCeleb-Bandage-Dress-H1520-Black\" width=\"525\" height=\"500\"><\/iframe><\/p>",
+		"tags": ["pcat: Bandage Dress-scat:", "all", "pcat:Color-scat:black", "iCeleb-Bandage-Dress-H1520-Black", "colour:black"],
+		"images": {},
+		"sort": {
+			"s1": null,
+			"s2": null,
+			"s3": null,
+			"s4": 44.95
+		},
+		"foot": "",
+		"onopenparam": {
+			"title": "TROPICAL SHIRTS MF04",
+			"tii_title": "TROPICAL SHIRT MF04",
+			"description": "",
+			"video": [],
+			"url": "http:\/\/ifashion.co\/products\/iCeleb-Bandage-Dress-H1520-Black\/",
+			"carousel_images": [],
+			"cart_url": null,
+			"product_uid": "MF04"
+		},
+		"compiledImg": "avi.png"
+	},
+
+
+	{
+		"uid": "MF04",
+		"title": "<p>TROPICAL SHIRTS MF04<\/p>",
+		"custom5":
+		"<p><iframe src=\"https:\/\/ifashion.co\/products\/Sky-Blye-And-Nude-Long-Sleeve-Crop-Top-Two-Piece-Dress-HLBD777\" width=\"525\" height=\"500\"><\/iframe><\/p>",
+		"description": "<p><iframe src=\"https:\/\/ifashion.co\/products\/iCeleb-Bandage-Dress-H1520-Black\" width=\"525\" height=\"500\"><\/iframe><\/p>",
+		"tags": ["pcat: Bandage Dress-scat:", "all", "pcat:Color-scat:black", "iCeleb-Bandage-Dress-H1520-Black", "colour:black"],
+		"images": {},
+		"sort": {
+			"s1": null,
+			"s2": null,
+			"s3": null,
+			"s4": 44.95
+		},
+		"foot": "",
+		"onopenparam": {
+			"title": "TROPICAL SHIRTS MF04",
+			"tii_title": "TROPICAL SHIRT MF04",
+			"description": "",
+			"video": [],
+			"url": "http:\/\/ifashion.co\/products\/iCeleb-Bandage-Dress-H1520-Black\/",
+			"carousel_images": [],
+			"cart_url": null,
+			"product_uid": "MF04"
+		},
+		"compiledImg": "avi.png"
+	},
+
+
+		{
+			"uid": "MF02",
+			"title": "<p>TROPICAL SHIRTS MF04<\/p>",
+			"custom5":
+			"<p><iframe src=\"https:\/\/ifashion.co\/products\/Sky-Blye-And-Nude-Long-Sleeve-Crop-Top-Two-Piece-Dress-HLBD777\" width=\"525\" height=\"500\"><\/iframe><\/p>",
+			"description": "<p><iframe src=\"https:\/\/ifashion.co\/products\/iCeleb-Bandage-Dress-H1520-Black\" width=\"525\" height=\"500\"><\/iframe><\/p>",
+			"tags": ["pcat: Bandage Dress-scat:", "all", "pcat:Color-scat:black", "iCeleb-Bandage-Dress-H1520-Black", "colour:black"],
+			"images": {},
+			"sort": {
+				"s1": null,
+				"s2": null,
+				"s3": null,
+				"s4": 44.95
+			},
+			"foot": "",
+			"onopenparam": {
+				"title": "TROPICAL SHIRTS MF02",
+				"tii_title": "TROPICAL SHIRT MF02",
+				"description": "",
+				"video": [],
+				"url": "http:\/\/ifashion.co\/products\/iCeleb-Bandage-Dress-H1520-Black\/",
+				"carousel_images": [],
+				"cart_url": null,
+				"product_uid": "MF04"
+			},
+			"compiledImg": "avi.png"
+		},
+		{
+			"uid": "MF03",
+			"title": "<p>TROPICAL SHIRTS MF03<\/p>",
+			"custom5":
+			"<p><iframe src=\"https:\/\/ifashion.co\/products\/Sky-Blye-And-Nude-Long-Sleeve-Crop-Top-Two-Piece-Dress-HLBD777\" width=\"525\" height=\"500\"><\/iframe><\/p>",
+			"description": "<p><iframe src=\"https:\/\/ifashion.co\/products\/iCeleb-Bandage-Dress-H1520-Black\" width=\"525\" height=\"500\"><\/iframe><\/p>",
+			"tags": ["pcat: Bandage Dress-scat:", "all", "pcat:Color-scat:black", "iCeleb-Bandage-Dress-H1520-Black", "colour:black"],
+			"images": {},
+			"sort": {
+				"s1": null,
+				"s2": null,
+				"s3": null,
+				"s4": 44.95
+			},
+			"foot": "",
+			"onopenparam": {
+				"title": "TROPICAL SHIRTS MF03",
+				"tii_title": "TROPICAL SHIRT MF03",
+				"description": "",
+				"video": [],
+				"url": "http:\/\/ifashion.co\/products\/iCeleb-Bandage-Dress-H1520-Black\/",
+				"carousel_images": [],
+				"cart_url": null,
+				"product_uid": "MF04"
+			},
+			"compiledImg": "avi.png"
+		},
+		{
+			"uid": "MF04",
+			"title": "<p>TROPICAL SHIRTS MF04<\/p>",
+			"custom5":
+			"<p><iframe src=\"https:\/\/ifashion.co\/products\/Sky-Blye-And-Nude-Long-Sleeve-Crop-Top-Two-Piece-Dress-HLBD777\" width=\"525\" height=\"500\"><\/iframe><\/p>",
+			"description": "<p><iframe src=\"https:\/\/ifashion.co\/products\/iCeleb-Bandage-Dress-H1520-Black\" width=\"525\" height=\"500\"><\/iframe><\/p>",
+			"tags": ["pcat: Bandage Dress-scat:", "all", "pcat:Color-scat:black", "iCeleb-Bandage-Dress-H1520-Black", "colour:black"],
+			"images": {},
+			"sort": {
+				"s1": null,
+				"s2": null,
+				"s3": null,
+				"s4": 44.95
+			},
+			"foot": "",
+			"onopenparam": {
+				"title": "TROPICAL SHIRTS MF04",
+				"tii_title": "TROPICAL SHIRT MF04",
+				"description": "",
+				"video": [],
+				"url": "http:\/\/ifashion.co\/products\/iCeleb-Bandage-Dress-H1520-Black\/",
+				"carousel_images": [],
+				"cart_url": null,
+				"product_uid": "MF04"
+			},
+			"compiledImg": "avi.png"
+		}
+	],
+	"coloursPresent": ["Pink", "Grey", "Red", "Blue", "Yellow", "Green", "Gold", "Gray", "Brown", "White", "Black"]
+}
