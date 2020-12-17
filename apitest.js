ecfa2e8ba72b28b3ad4fa263cf6a369491aff661
avibenfashion <59139276+avibenfashion@users.noreply.github.com>
2020-12-17 07:58:57 +0200

    Update apitest.js

--- apitest.js
+++ apitest.js
@@ -42,7 +42,52 @@
 			"s3": null,
 			"s4": 44.95
 		},
-		"foot": "",
+		"foot":TII_PRODUCTS = {
+	"results" : [
+        {
+            "uid": "MF01",
+            "title": "<p>TROPICAL SHIRTS</p>",
+            "custom5":
+              '<p><iframe src="http://127.0.0.1:5500/ifashion.html" width="100%" height="200" ></iframe>',
+              "description":
+              '<p><iframe src="http://127.0.0.1:5500/ifashion.html" width="100%" height="500"></iframe></p>',
+              "tags": [
+              "pcat:All",
+              "pcat:FASHION-scat:Shirts",
+              "all",
+              "Price:50",
+              "MF01",
+              "colour:Gray",
+            ], 
+            "images": {},
+            "sort": {
+              "Date": "2020-09-01",
+              "Title": "TROPICAL SHIRTS",
+              "Price": 1,
+              "s1": 0,
+              "s2": 0,
+              "s3": 95,
+              /*price slider price sort is s4*/
+              "s4": 50,
+            },
+            "foot": "",
+            "onopenparam": {
+              "title": "TROPICAL SHIRTS",
+              "tii_title": "TROPICAL SHIRTS",
+              "description": "",
+              "video": [],
+              "video_poster": "",
+              "carousel_images": [{}],
+              "json_sub_products": [],
+              "cart_url": null,
+              "product_uid": "MF01",
+            },
+            "compiledImg": "avi.png",
+          }
+        ],
+	"coloursPresent": ["Pink", "Grey", "Red", "Blue", "Yellow", "Green", "Gold", "Gray", "Brown", "White", "Black"]
+}
+
 		"onopenparam": {
 			"title": "TROPICAL SHIRTS MF04",
 			"tii_title": "TROPICAL SHIRT MF04",
